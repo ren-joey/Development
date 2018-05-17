1d450f387db571c39e15bd51d182a0cd9e758ce3
TsuYi Ren <tsuyiren@gmail.com>
2018-05-17 23:15:33 +0800

    201805172315 windows

---
+++ vue/retry/vue_basic.js
@@ -0,0 +1,8 @@
+const A = {
+        props: ['title'],
+        template: '<div class="A">{{ title }}</div>'
+    },
+    B = {
+        props: ['title'],
+        template: '<div class="B">{{ title }}</div>'
+    };
